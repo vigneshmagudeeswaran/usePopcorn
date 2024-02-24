c7824c16043c655179b057177679fffc57d8f31f
Vignesh Magudeeswaran <m.vigneshviki1996@gmail.com>
2024-02-25 02:02:06 +0530

    usePopCorn

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={5} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      size={24}
+      color={"red"}
+      messages={["terrible", "bad", "okay", "Good", "Excellent"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} />
+    <Test />
+  </React.StrictMode>
+);
